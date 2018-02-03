85226d7e8c6befe748c7f5033b51824b868c025c
Calvin <calvin.cheng@stonybrook.edu>
2018-02-03 17:39:33 -0500

    mod2.1.8

--- src/App.js
+++ src/App.js
@@ -11,8 +11,35 @@ class App extends Component {
 				{description : "Take out garbage", isCompleted : false},
 				{description : "Buy groceries", isCompleted : true},
 				{description : "Do homework", isCompleted : false},
-			]
+			],
+			newDescription: ""
 		};
+	}
+	
+	toggleComplete(index){
+		var newToDo = this.state.todos;
+		console.log(newToDo.length);
+		newToDo[index].isCompleted = !newToDo[index].isCompleted
+		this.setState({
+			todos : newToDo
+		});
+	}
+	
+	handleSubmit(e){
+		e.preventDefault();
+		if(!this.state.newDescription){
+			return;
+		}
+		const newToDo = {description:this.state.newDescription, isCompleted:false};
+		this.setState({
+			todos: [...this.state.todos, newToDo],
+			newDescription: ""
+		});
+	}
+	handleChange(e){
+		this.setState({
+			newDescription : e.target.value 
+		});
 	}
 	
   render() {
@@ -20,9 +47,13 @@ class App extends Component {
       <div className="App">
         <ul>
         	{this.state.todos.map( (todo,index) => 
-        		<ToDo key={index} description={todo.description} isCompleted = {todo.isCompleted}/>
+        		<ToDo key={index} description={todo.description} isCompleted = {todo.isCompleted} toggleComplete = {() => this.toggleComplete(index)}/>
         	)}
         </ul>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+        	<input type="text" value={this.state.newDescription} onChange={ (e) => this.handleChange(e)} />
+        	<input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,7 +6,7 @@
  	
  		return (
  			<div>
- 				<input type="checkbox" checked={this.props.isCompleted} />
+ 				<input type="checkbox" checked={this.props.isCompleted} onChange ={this.props.toggleComplete} />
  				<span>{this.props.description} </span>
  			</div>
  		);
